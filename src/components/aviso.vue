3e1737e571e887f3a329c4f0ec16dad0539e2787
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-07 20:46:18 -0300

    Update aviso.vue
    Alterações no botão.

--- src/components/aviso.vue
+++ src/components/aviso.vue
@@ -2,7 +2,7 @@
         <div id="caixa">
             <div id="alocar">
                 <h5>Agora, iremos tentar expressar o que sentimos com as cores que iremos ver.</h5>
-                <a href="/vermelho.html"><b-button>Seguinte</b-button></a>
+                <div align="right"><a href="/vermelho.html"><button class="small">Seguinte</button></a></div>            
             </div>
         </div>  
 </template>
@@ -15,18 +15,21 @@ export default {
 <style scoped>
 #caixa {
   border: 100px solid transparent;
-  border-image: url(img/caixinha.png) 95 stretch;
+  border-image: url(img/balao1.png) 250 stretch;
   margin-right: 65%;
   position: absolute;
 }
 
 #alocar{
   position: relative;
-  top: -30px;
+  top: -10px;
+  left: -15px;
 }
 
 h5{
-    font-family: "Comic Sans MS";
+  font-family: "Comic Sans MS";
+  font-size: medium;
+  padding: 10px;
 }
 </style>
 
