0e8e6bec7336e0e4ca996c83260c97438ee107cd
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-06 21:15:20 -0300

    Update fala1.vue
    Alterações no botão de seguinte e alterações no css

--- src/components/fala1.vue
+++ src/components/fala1.vue
@@ -3,7 +3,7 @@
   <div id="caixa1">
     <div id="alocar1">
       <h5>Tenho 9 anos, nasci na cidade de Rio Tinto, na Universidade Federal da Paraíba do campus IV.</h5>
-      <router-link to="/fala2"><b-button>Seguinte</b-button></router-link>
+      <router-link to="/fala2"><button>Seguinte</button></router-link>
     </div>
   </div>
 </body>
@@ -14,36 +14,6 @@
 </script>
 
 <style scoped>
-.container {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.box {
-  width: 300px;
-  height: 300px;
-}
-
-.borda {
-  border: 10px solid black;
-  border-radius: 30px;
-  padding: 3%;
-}
-
-input {
-  border: solid 1px solid;
-  border-radius: 15px;
-  height: 20px;
-  display: block;
-}
-
-#input-small {
-  border: 3px solid;
-  border-radius: 30px;
-}
-
 #caixa1 {
   border: 100px solid transparent;
   border-image: url(img/caixinha.png) 95 stretch;
