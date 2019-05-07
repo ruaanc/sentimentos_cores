24130d4826bd6e7a19f15b9202f16a02eb5657f8
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-07 20:05:48 -0300

    Update idade.vue
    Atualização do arquivo idade.vue.

--- src/components/idade.vue
+++ src/components/idade.vue
@@ -3,12 +3,10 @@
   <div id="caixa5">
     <div id="alocar5">
       <h5>Qual sua idade?</h5>
-        <div>
+        <h5>
             <b-form-input v-model="text" placeholder="Digite"></b-form-input>
-            <div class="mt-2">Value: {{ text }}</div>
-        </div>
-      <br>
-      <router-link to="/cor_preferida"><b-button>Seguinte</b-button></router-link>
+        </h5>
+      <div align="right"><router-link to="/idade"><button class="small">Seguinte</button></router-link></div>
     </div>
   </div>
 </body>
@@ -27,42 +25,24 @@
 </script>
 
 <style scoped>
-.container {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.box {
-  width: 300px;
-  height: 300px;
-}
-
-.borda {
-  border: 10px solid black;
-  border-radius: 30px;
-  padding: 3%;
-}
-
-#input-small {
-  border: 3px solid;
-  border-radius: 30px;
-}
 
 #caixa5 {
   border: 100px solid transparent;
-  border-image: url(img/caixinha.png) 95 stretch;
+  border-image: url(img/balao1.png) 250 stretch;
   margin-right: 65%;
   position: absolute;
 }
 
 #alocar5{
   position: relative;
-  top: -30px;
+  top: -15px;
+  left: -15px;
 }
 
 h5{
-    font-family: "Comic Sans MS";
+  font-family: "Comic Sans MS";
+  font-size: medium;
+  padding: 10px;
+  margin-left: - 10px;
 }
 </style>
