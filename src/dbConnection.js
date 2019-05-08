6d9979487fad78ddc89f3ae8080cdcdfd319ddaf
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-08 20:38:23 -0300

    Create dbConnection.js
    Criação da conexão com o banco de dados.

---
+++ src/dbConnection.js
@@ -0,0 +1,16 @@
+
+var mysql = require('mysql');
+var connMySQL = function(){
+
+        console.log("Conecao com bd foi estabelecida");
+       return mysql.createConnection({
+            host : '',
+            user : 'root',
+            password : 'sisalfa',
+            database : 'portal_noticias'
+        });
+}
+module.exports = function(){
+    console.log("o autoload carregou o modulo de conexao com banco de dados");
+    return connMySQL;
+}
