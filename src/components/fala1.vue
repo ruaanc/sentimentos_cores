aea50f3352dfaf337a1432c0383e6cc27fd13994
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-08 20:36:59 -0300

    Update fala1.vue
    Alteração do grid e adição da animação.

--- src/components/fala1.vue
+++ src/components/fala1.vue
@@ -1,11 +1,23 @@
 <template>
 <body id="cor">
-  <div id="caixa1">
-    <div id="alocar1">
-      <h5>Tenho 9 anos, nasci na cidade de Rio Tinto, na Universidade Federal da Paraíba do campus IV.</h5>
-      <div align="right"><router-link to="/fala2"><button class="small">Seguinte</button></router-link></div>
-    </div>
-  </div>
+  <b-container class="bv-example-row">
+    <b-row>
+      <div id="en">
+          <div id="caixa1">
+            <div id="alocar1">
+              <h5>Tenho 9 anos, nasci na cidade de Rio Tinto, na Universidade Federal da Paraíba do campus IV.</h5>
+              <div align="right"><router-link to="/fala2"><button class="small">Seguinte</button></router-link></div>
+            </div>
+          </div>
+      </div>
+    </b-row>
+  </b-container>  
+  <b-container class="bv-example-row">
+  <b-row>
+    <div id="in"><b-col><div class="runner"></div></b-col></div>
+  </b-row>
+</b-container>
+  
 </body>
 </template>
 
@@ -17,7 +29,7 @@
 #caixa1 {
   border: 100px solid transparent;
   border-image: url(img/balao1.png) 250 stretch;
-  margin-right: 65%;
+  margin-right: 500px;
   position: absolute;
 }
 
@@ -30,5 +42,24 @@ h5{
   font-family: "Comic Sans MS";
   font-size: medium;
   padding: 10px;
+}
+.runner{
+  width: 150px;
+  height: 200px;
+  background: url(img/enzo.png);
+  background-size: 980px;
+  animation: run 1100ms steps(4);
+  padding-top: 280px;
+  padding-right: 20px;
+  padding-left: 200px;
+}
+@keyframes run {
+  100% { background-position: -17650px; }
+}
+#en{
+  padding-left: 200px;
+}
+#in{
+  padding-top: 250px;
 }
 </style>
