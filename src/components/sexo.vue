ae46cbdc4415eef45a82965fd766e323f615af5c
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-09 02:34:28 -0300

    Update sexo.vue

--- src/components/sexo.vue
+++ src/components/sexo.vue
@@ -1,6 +1,9 @@
 <template>
 <body id="cor">
-  <div id="caixa4">
+ <b-container class="bv-example-row">
+  <b-row>
+  <div id="en">
+   <div id="caixa4">
     <div id="alocar4">
       <h5>Você é do sexo masculino ou feminino?</h5>
         <h5>
@@ -11,7 +14,15 @@
         </h5>     
       <div align="right"><router-link to="/idade"><button class="small">Seguinte</button></router-link></div>
     </div>
+   </div>
   </div>
+  </b-row>
+ </b-container> 
+ <b-container class="bv-example-row">
+  <b-row>
+    <div id="in"><b-col><div class="runner"></div></b-col></div>
+  </b-row>
+ </b-container>
 </body>
 </template>
 
@@ -32,7 +43,7 @@
 #caixa4 {
   border: 100px solid transparent;
   border-image: url(img/balao1.png) 250 stretch;
-  margin-right: 65%;
+  margin-right: 500px;
   position: absolute;
 }
 
@@ -47,5 +58,39 @@ h5{
   font-size: medium;
   padding: 5px;
   margin-left: - 10px;
+}
+.runner{
+  width: 200px;
+  height: 330px;
+  background: url(img/1.png);
+  background-size: 200px;
+  animation-name: run;
+  animation-duration: 700ms;
+  animation-iteration-count: 2;
+}
+@keyframes run {
+  0% {
+    background: url(img/2.png);
+    background-size: 200px;
+    }
+  25% {
+    background: url(img/3.png);
+    background-size: 200px;
+    }
+  50%  {
+    background: url(img/4.png);
+    background-size: 200px;
+    }
+  100% {
+    background: url(img/5.png);
+    background-size: 200px;
+    }
+  
+}
+#en{
+  padding-left: 200px;
+}
+#in{
+  padding-top: 350px;
 }
 </style>
