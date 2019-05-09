8e12d5c8f76ea73e2a835fcee0a085767a059548
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-09 02:33:44 -0300

    Update fala2.vue

--- src/components/fala2.vue
+++ src/components/fala2.vue
@@ -1,11 +1,22 @@
 <template>
 <body id="cor">
-  <div id="caixa2">
-    <div id="alocar2">
-      <h5>Eu gosto muito das cores, porque elas conseguem transmitir sentimentos.</h5>
-      <div align="right"><router-link to="/fala3"><button class="small">Seguinte</button></router-link></div>
-    </div>
-  </div>
+  <b-container class="bv-example-row">
+    <b-row>
+      <div id="en">
+          <div id="caixa2">
+            <div id="alocar2">
+              <h5>Eu gosto muito das cores, porque elas conseguem transmitir sentimentos.</h5>
+              <div align="right"><router-link to="/fala3"><button class="small">Seguinte</button></router-link></div>
+            </div>
+          </div>
+      </div>
+    </b-row>
+  </b-container>  
+  <b-container class="bv-example-row">
+  <b-row>
+    <div id="in"><b-col><div class="runner"></div></b-col></div>
+  </b-row>
+</b-container>
 </body>
 </template>
 
@@ -17,7 +28,7 @@
 #caixa2 {
   border: 100px solid transparent;
   border-image: url(img/balao1.png) 250 stretch;
-  margin-right: 65%;
+  margin-right: 500px;
   position: absolute;
 }
 
@@ -31,5 +42,39 @@ h5{
   font-family: "Comic Sans MS";
   font-size: medium;
   padding: 10px;
+}
+.runner{
+  width: 200px;
+  height: 330px;
+  background: url(img/1.png);
+  background-size: 200px;
+  animation-name: run;
+  animation-duration: 700ms;
+  animation-iteration-count: 4;
+}
+@keyframes run {
+  0% {
+    background: url(img/2.png);
+    background-size: 200px;
+    }
+  25% {
+    background: url(img/3.png);
+    background-size: 200px;
+    }
+  50%  {
+    background: url(img/4.png);
+    background-size: 200px;
+    }
+  100% {
+    background: url(img/5.png);
+    background-size: 200px;
+    }
+  
+}
+#en{
+  padding-left: 200px;
+}
+#in{
+  padding-top: 250px;
 }
 </style>
