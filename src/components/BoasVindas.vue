aefecf9580627b2f7c5ae61e3473d5348d41143f
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-09 02:33:28 -0300

    Update BoasVindas.vue

--- src/components/BoasVindas.vue
+++ src/components/BoasVindas.vue
@@ -1,15 +1,23 @@
 <template>
 <div id="esquerda">
-  <div id="caixa">
-    <div id="alocar">
-      <div id="direita">
-        <h5>Olá, O meu nome é Enzo!</h5>
-        <div align="right"><router-link to="/fala1"><button class="small">Seguinte</button></router-link></div>
+   <b-container class="bv-example-row">
+    <b-row>
+      <div id="en">
+          <div id="caixa">
+            <div id="alocar">
+              <h5>Olá, O meu nome é Enzo!</h5>
+              <div align="right"><router-link to="/fala1"><button class="small">Seguinte</button></router-link></div>
+            </div>
+          </div>
       </div>
-    </div>
-  </div>
+    </b-row>
+  </b-container>  
+  <b-container class="bv-example-row">
+  <b-row>
+    <div id="in"><b-col><div class="runner"></div></b-col></div>
+  </b-row>
+</b-container>
 </div>
-
 </template>
 
 <script>
@@ -24,7 +32,7 @@ export default {
 #caixa {
   border: 100px solid transparent;
   border-image: url(img/balao1.png) 250 stretch;
-  margin-right: 70%;
+  margin-right: 500px;
   position: absolute;
 }
 
@@ -37,5 +45,39 @@ h5{
     font-family: "Comic Sans MS";
     font-size: medium;
     padding: 10px;
+}
+.runner{
+  width: 200px;
+  height: 330px;
+  background: url(img/1.png);
+  background-size: 200px;
+  animation-name: run;
+  animation-duration: 700ms;
+  animation-iteration-count: 2;
+}
+@keyframes run {
+  0% {
+    background: url(img/2.png);
+    background-size: 200px;
+    }
+  25% {
+    background: url(img/3.png);
+    background-size: 200px;
+    }
+  50%  {
+    background: url(img/4.png);
+    background-size: 200px;
+    }
+  100% {
+    background: url(img/5.png);
+    background-size: 200px;
+    }
+  
+}
+#en{
+  padding-left: 200px;
+}
+#in{
+  padding-top: 250px;
 }
 </style>
