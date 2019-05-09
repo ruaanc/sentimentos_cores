0c52dd5795d562e620a3aa119d9ab3277e249f4a
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-09 02:33:34 -0300

    Update Cor_preferida.vue

--- src/components/Cor_preferida.vue
+++ src/components/Cor_preferida.vue
@@ -1,5 +1,8 @@
 <template>
 <body id="boas">
+ <b-container class="bv-example-row">
+  <b-row>
+  <div id="en">
   <div id="caixa6">
     <div id="alocar6">
       <h5>Qual sua cor preferida?</h5>
@@ -13,6 +16,14 @@
       <div align="right"><router-link to="/aviso"><button class="small">Seguinte</button></router-link></div>
     </div>
   </div>
+  </div>
+  </b-row>
+  </b-container> 
+ <b-container class="bv-example-row">
+  <b-row>
+    <div id="in"><b-col><div class="runner"></div></b-col></div>
+  </b-row>
+ </b-container> 
 </body>
 </template>
 
@@ -33,7 +44,7 @@ export default {
 #caixa6 {
   border: 100px solid transparent;
   border-image: url(img/balao1.png) 250 stretch;
-  margin-right: 65%;
+  margin-right: 500px;
   position: absolute;
 }
 
@@ -72,5 +83,38 @@ h5{
   font-size: medium;
   padding: 5px;
   margin-left: - 10px;
+}
+.runner{
+  width: 200px;
+  height: 330px;
+  background: url(img/1.png);
+  background-size: 200px;
+  animation-name: run;
+  animation-duration: 700ms;
+  animation-iteration-count: 1;
+}
+@keyframes run {
+  0% {
+    background: url(img/2.png);
+    background-size: 200px;
+    }
+  25% {
+    background: url(img/3.png);
+    background-size: 200px;
+    }
+  50%  {
+    background: url(img/4.png);
+    background-size: 200px;
+    }
+  100% {
+    background: url(img/5.png);
+    background-size: 200px;
+    }
+}
+#en{
+  padding-left: 200px;
+}
+#in{
+  padding-top: 300px;
 }
 </style>
