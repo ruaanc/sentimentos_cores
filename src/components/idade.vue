226ddda0eebd050e2334343298bc7d501db66860
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-09 02:33:54 -0300

    Update idade.vue

--- src/components/idade.vue
+++ src/components/idade.vue
@@ -1,14 +1,32 @@
 <template>
 <body id="cor">
-  <div id="caixa5">
-    <div id="alocar5">
+  <b-container class="bv-example-row">
+  <b-row>
+   <div id="en">
+    <div id="caixa5">
+     <div id="alocar5">
       <h5>Qual sua idade?</h5>
         <h5>
-            <b-form-input v-model="text" placeholder="Digite"></b-form-input>
+          <b-form-group label="Escolha uma das opções: ">
+           <div id="7"><b-form-radio v-model="selected" name="some-radios" value="vermelhor"><font color="">7</font></b-form-radio></div>
+           <div id="8"><b-form-radio v-model="selected" name="some-radios" value="verde"><font color="">8</font></b-form-radio></div>
+           <div id="9"><b-form-radio v-model="selected" name="some-radios" value="azul"><font color="">9</font></b-form-radio></div>
+           <div id="10"><b-form-radio v-model="selected" name="some-radios" value="azul"><font color="">10</font></b-form-radio></div>
+           <div id="11"><b-form-radio v-model="selected" name="some-radios" value="azul"><font color="">11</font></b-form-radio></div>
+           <div id="12"><b-form-radio v-model="selected" name="some-radios" value="azul"><font color="">12</font></b-form-radio></div>
+          </b-form-group>
         </h5>
       <div align="right"><router-link to="/cor_preferida"><button class="small">Seguinte</button></router-link></div>
+     </div>
     </div>
-  </div>
+   </div>
+  </b-row>
+ </b-container>
+ <b-container class="bv-example-row">
+  <b-row>
+    <div id="in"><b-col><div class="runner"></div></b-col></div>
+  </b-row>
+ </b-container> 
 </body>
 </template>
 
@@ -17,7 +35,7 @@
   export default {
     data() {
       return {
-        text: ''
+        selected: ''
       }
     }
   }
@@ -29,7 +47,7 @@
 #caixa5 {
   border: 100px solid transparent;
   border-image: url(img/balao1.png) 250 stretch;
-  margin-right: 65%;
+  margin-right: 500px;
   position: absolute;
 }
 
@@ -44,5 +62,39 @@ h5{
   font-size: medium;
   padding: 10px;
   margin-left: - 10px;
+}
+.runner{
+  width: 200px;
+  height: 330px;
+  background: url(img/1.png);
+  background-size: 200px;
+  animation-name: run;
+  animation-duration: 700ms;
+  animation-iteration-count: 1;
+}
+@keyframes run {
+  0% {
+    background: url(img/2.png);
+    background-size: 200px;
+    }
+  25% {
+    background: url(img/3.png);
+    background-size: 200px;
+    }
+  50%  {
+    background: url(img/4.png);
+    background-size: 200px;
+    }
+  100% {
+    background: url(img/5.png);
+    background-size: 200px;
+    }
+  
+}
+#en{
+  padding-left: 200px;
+}
+#in{
+  padding-top: 300px;
 }
 </style>
