355d85273593e5c424aa218122821366cef417fa
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-09 02:33:49 -0300

    Update fala3.vue

--- src/components/fala3.vue
+++ src/components/fala3.vue
@@ -1,11 +1,22 @@
 <template>
 <body id="cor">
-  <div id="caixa3">
-    <div id="alocar3">
-      <h5>Agora eu quero conhecer mais sobre você.</h5>
-      <div align="right"><router-link to="/sexo"><button class="small">Seguinte</button></router-link></div>
-    </div>
-  </div>
+  <b-container class="bv-example-row">
+    <b-row>
+      <div id="en">
+          <div id="caixa3">
+            <div id="alocar3">
+              <h5>Agora eu quero conhecer mais sobre você.</h5>
+              <div align="right"><router-link to="/sexo"><button class="small">Seguinte</button></router-link></div>
+            </div>
+          </div>
+      </div>
+    </b-row>
+  </b-container>  
+  <b-container class="bv-example-row">
+  <b-row>
+    <div id="in"><b-col><div class="runner"></div></b-col></div>
+  </b-row>
+</b-container>
 </body>
 </template>
 
@@ -18,7 +29,7 @@
 #caixa3 {
   border: 100px solid transparent;
   border-image: url(img/balao1.png) 250 stretch;
-  margin-right: 70%;
+  margin-right: 500px;
   position: absolute;
 }
 
@@ -32,5 +43,39 @@ h5{
   font-family: "Comic Sans MS";
   font-size: medium;
   padding: 10px;
+}
+.runner{
+  width: 200px;
+  height: 330px;
+  background: url(img/1.png);
+  background-size: 200px;
+  animation-name: run;
+  animation-duration: 700ms;
+  animation-iteration-count: 3;
+}
+@keyframes run {
+  0% {
+    background: url(img/2.png);
+    background-size: 200px;
+    }
+  25% {
+    background: url(img/3.png);
+    background-size: 200px;
+    }
+  50%  {
+    background: url(img/4.png);
+    background-size: 200px;
+    }
+  100% {
+    background: url(img/5.png);
+    background-size: 200px;
+    }
+  
+}
+#en{
+  padding-left: 200px;
+}
+#in{
+  padding-top: 250px;
 }
 </style>
